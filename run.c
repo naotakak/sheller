01b94e7032e3c797e497eabc9f4578f08e10fdec
Cynthia Cheng <cynthiacheng1022@gmail.com>
2017-12-02 22:04:40 -0500

    need to testmore

--- run.c
+++ run.c
@@ -1,5 +1,44 @@
 #include "headers.h"
 
+void redirect(char* command){
+  char** commands = space_sep(command)
+  int in, out;
+  in = open("stdin.txt", O_RDONLY);
+  out = open("stdout.txt", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+  dup2(in, 0);
+  dup2(out, 1);
+  close(in);
+  close(out);
+  execvp(commands[0],commands);
+}
+
+void pipe(char* command){
+  char ** commands = calloc(10, sizeof(char *));
+  while((commands[i] = strsep(&args, "|"))) {
+    i ++;
+  }
+  char *first[] = space_sep(commands[0]);
+  char *second[] = space_sep(commands[1]);
+
+  int inputforpipe[2]
+  int pid;
+
+  pipe(inputforpipe);
+  pid = fork();
+
+  if (pid == 0){ //child does second command
+    dup2(inputforpipe[0], 0);
+    //closes the other half
+    close(inputforpipe[1]);
+    execvp(first[0],first);
+  }
+  else{
+    dup2(inputforpipe[1], 1);
+    close(inputforpipe[0]);
+    execvp(second[0],second);
+  }
+}
+
 char ** colonoscopy(char * args) {
   /***
       Make commands separated by ';' into different strings and then run them
